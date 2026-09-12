c752b72a19d842ca2a7c8e9cefffbdd52148327c
eskezje <eskebn@gmail.com>
2026-09-12 16:59:13 +0200

    parsing

--- src/core/nvram_parser.cpp
+++ src/core/nvram_parser.cpp
@@ -1,10 +1,13 @@
 #include "nvram_parser.hpp"
 
 #include <fstream>
+#include <optional>
 #include <regex>
 #include <stdexcept>
+#include <string>
 
 namespace {
+
 struct ParserRegex {
   std::regex comment;
   std::regex setup_question;
@@ -15,6 +18,8 @@ struct ParserRegex {
   std::regex bios_default;
   std::regex options;
   std::regex value;
+  std::regex bracket_option;
+  std::regex option;
 };
 
 const ParserRegex patterns{
@@ -29,7 +34,20 @@ const ParserRegex patterns{
     .bios_default =
         std::regex(R"(^BIOS\s+Default\s*=\s*(.*))", std::regex::icase),
     .options = std::regex(R"(^Options\s*=\s*(.*))", std::regex::icase),
-    .value = std::regex(R"(^Value\s*=\s*(.*))", std::regex::icase)};
+    .value = std::regex(R"(^Value\s*=\s*(.*))", std::regex::icase),
+    .bracket_option = std::regex(R"(^\**\[(.*?)\](.*))"),
+    .option = std::regex(R"((\*?)\[([^\]]+)\](\S.*?|)$)")};
+
+std::string trim(const std::string& str)
+{
+    const auto first = str.find_first_not_of(" \t\r\n");
+    if (first == std::string::npos) {
+        return "";
+    }
+    const auto last = str.find_last_not_of(" \t\r\n");
+    return str.substr(first, last - first + 1);
+}
+
 } // namespace
 
 std::vector<Setting> parse_nvram(const std::string &path) {
@@ -37,10 +55,24 @@ std::vector<Setting> parse_nvram(const std::string &path) {
   std::ifstream file(path);
   // check if file could open
   if (!file.is_open()) {
-    throw std::runtime_error("could not open file");
+    throw std::runtime_error("could not open file" + path);
   }
   std::vector<Setting> settings;
+  std::optional<Setting> current_setting;
   // parsing here
+  std::string line;
+  std::smatch match;
+
+  while (std::getline(file,line)) {
+    // removing comment
+    line = std::regex_replace(line, patterns.comment, "");
+    // remove whitespace at front and at end
+    line = trim(line);
+    // setup question 
+    if (std::regex_match(line, match, patterns.setup_question)) {
+      // handle the setting 
+    }
+  }
 
   return settings;
 }
